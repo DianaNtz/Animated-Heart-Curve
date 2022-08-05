ff3e7dc5d577ddf6cfe618f1412f6a1680a8101d
Diana Nitzschke <diana.nitzschke@stud.uni-hannover.de>
2022-08-05 14:56:22 +0200

    added code description and some comments

--- imageioexample.py
+++ imageioexample.py
@@ -1,14 +1,16 @@
 """
-@author: Diana Nitzschke
+The code below was written by @author: https://github.com/DianaNtz and is an 
+example on how to create an animated gif with the library imageio.
 """
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import imageio
-n=100 
+n=100       #gifs worth in units of 10**3 words :)
 t=np.linspace(-1,1,n-1)
 filenames = []
 for i in range(0,n):
+    #plot the curve
     fig, ax = plt.subplots(figsize=(7,7))
     ax.plot(t[:i],np.sqrt((t[:i])**2)+np.sqrt(1-(t[:i])**2),"-",
     linewidth=3.0,color='deeppink',label='$(\:t,|t|+\sqrt{1-t^2}\:)$')
@@ -19,13 +21,18 @@ for i in range(0,n):
     plt.xticks(fontsize= 16) 
     plt.yticks(fontsize= 16)
     plt.legend(loc=1,fontsize= 16)
+    #create file name for index i 
     filename ='bla{0:.0f}.png'.format(i)
+    #append file name to the list filename
     filenames.append(filename)
+    #save the plot
     plt.savefig(filename,dpi=250)
     plt.close()
+#build the gif
 with imageio.get_writer('Herz.gif', mode='I') as writer:
     for filename in filenames:
         image = imageio.imread(filename)
         writer.append_data(image) 
+#remove saved figures 
 for filename in set(filenames):
     os.remove(filename)        
